c9efc9566c773f08166f5d2ac1f461eba48a129d
Uraniumus <irma121@ya.ru>
2019-09-12 08:26:35 +0300

    done 2/4, 7/10 tests passed

--- src/towns.js
+++ src/towns.js
@@ -37,6 +37,28 @@ const homeworkContainer = document.querySelector('#homework-container');
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  */
 function loadTowns() {
+      return townsPromise = new Promise((resolve) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.responseType = 'json';
+
+        xhr.addEventListener('load', () => {
+
+            loadingBlock.innerHTML = '';
+            filterBlock.style.display = 'inline-block';
+
+            resolve(xhr.response.sort(function (a, b) {
+                if (a.name > b.name) {
+                    return 1;
+                }
+                if (a.name < b.name) {
+                    return -1;
+                }
+                return 0;
+            }));
+        });
+        xhr.send();
+    });
 }
 
 /*
@@ -51,6 +73,9 @@ function loadTowns() {
    isMatching('Moscow', 'Moscov') // false
  */
 function isMatching(full, chunk) {
+      if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) 
+        return true;
+      return false;
 }
 
 /* Блок с надписью "Загрузка" */
